6185adce1156bbbe9d3ce6e6824037954ea130b4
yjl0912 <2382813371@qq.com>
2021-01-08 08:50:12 +0800

    feat:杨金亮和史帅乐第一次合并后解决了一个问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,23 @@
-<<<<<<< HEAD
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Home from "../views/home/home.vue"
 import GoodsDetail from '../views/GoodsDetail/index.vue'
 import Login from '../views/Login/index.vue'
+import PayDetail from "../views/PayDetail"
 
 
 // 重写push和replace方法
 // 目的：为了让编程式导航重复点击时不报错~
-=======
-import Vue from "vue";
-import VueRouter from "vue-router";
+
 Vue.use(VueRouter);
 
-import GoodsDetail from "../views/GoodsDetail";
-import PayDetail from "../views/PayDetail"
 
->>>>>>> 27a17fccae105a5736e66c9f91c634a3c8b4947e
+
+
 const push = VueRouter.prototype.push;
 const replace = VueRouter.prototype.replace;
 
 VueRouter.prototype.push = function(location, onComplete, onAbort) {
-<<<<<<< HEAD
 	// 如果用户想处理失败，就处理
 	if (onComplete && onAbort) {
 		return push.call(this, location, onComplete, onAbort);
@@ -59,27 +55,8 @@ VueRouter.prototype.replace = function(location, onComplete, onAbort) {
           {
               path:'/Login' ,
               component:Login
-          }
-     
-     ]
- })
- export default router
-=======
-  if (onComplete && onAbort) {
-    return push.call(this, location, onComplete, onAbort);
-  }
-  return push.call(this, location, onComplete, () => {});
-};
-VueRouter.prototype.replace = function(location, onComplete, onAbort) {
-  if (onComplete && onAbort) {
-    return replace.call(this, location, onComplete, onAbort);
-  }
-  return replace.call(this, location, onComplete, () => {});
-};
-
- const router =new VueRouter({
-    routes: [
-        {
+          },
+          {
             path:"/goodsdetail",
             component:GoodsDetail
         },
@@ -87,7 +64,10 @@ VueRouter.prototype.replace = function(location, onComplete, onAbort) {
             path:"/paydetail",
             component:PayDetail
         }
-    ]
+     
+     ]
  })
- export default router;
->>>>>>> 27a17fccae105a5736e66c9f91c634a3c8b4947e
+ export default router
+ 
+
+
